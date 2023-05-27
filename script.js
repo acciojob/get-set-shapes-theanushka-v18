a4b61f6d85b88d3d7cf0220e28f6d58f9d686496
Anushka Verma <anushkaverma779@gmail.com>
2023-05-27 20:23:02 +0530

    done

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-//complete this code
-class Rectangle {}
-
-class Square extends Animal {}
-
-// Do not change the code below this line
-window.Rectangle = Rectangle;
-window.Square = Square;
+class Rectangle {
+    constructor(width,height){
+        this._width = width
+        this._height = height
+    }
+    get width(){
+        return this._width;
+    }
+    get height(){
+        return this._height;
+    }
+    getArea(){
+        return this._width * this._height
+    }
+  }
+  
+  class Square extends Rectangle {
+    constructor(sidelength){
+        super(sidelength,sidelength);
+    }
+    getPerimeter(){
+        return 4*this._width;
+    }
+  }
+  
+  
+  // Do not change the code below this line
+  window.Rectangle = Rectangle;
+  window.Square = Square;
